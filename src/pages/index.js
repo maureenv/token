1ef9344b32299969d877ec145f6825ba9b31ccde
maureenv <maureenvogel@outlook.com>
2018-06-24 16:12:34 -0400

    Add wallet section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,23 +4,51 @@ import React from 'react'
 import Features from '../components/features'
 import fuck from '../../images/fuck.png'
 import fuckLogo from '../../images/fucktoken-circle-logo.png'
+import coinIcon from '../../images/icon-coins.png'
+import pieIcon from '../../images/icon-piechart.png'
+import fuckBlue from '../../images/fuck-blue.png'
+import laptop from '../../images/laptop.png'
 
 
 const IndexPage = props => (
-  <main>
+  <div>
     {/*<Features data={props.data.allDataJson.edges[0].node.features} />*/}
     <div className="hero">
       <div className="hero__inner">
         <img className="hero-logo" src={ fuckLogo } alt="Fuck token logo"/>
         <h1> Express yourself, give a <img src={ fuck } alt="fuck"/></h1>
         <div className="button-container">
-          <button className="button button-light"> Enter Fuckhub </button>
-          <button className="button button-dark"> Get Tokens</button>
+          <button className="button button-light pie-icon"><img src={ pieIcon } alt="pie icon"/><p>Enter Fuckhub </p></button>
+          <button className="button button-dark coin-icon"><img src={ coinIcon } alt="coin icon"/><p>Get Tokens</p></button>
         </div>
       </div>
     </div>
 
-  </main>
+    <div className="what-container">
+      <div className="what-container__inner">
+        <div className="video-container">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/s_NiGNa6XmA?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div className="what-description">
+          <h2> What is <img src={ fuckBlue } alt="fuck"/> Token?</h2>
+          <p><em>FUCK Token</em> is an Ethereum-based tipping token for mainstream social media and chat websites.  Our token enables real-time peer-to-peer value exchange, while allowing users to express their opinions.  <em>Giving a FUCK</em> is similar to an upvote, like, or favorite.</p>
+        </div>
+      </div>
+      </div>
+      <div className="wallet-container">
+        <div className="wallet-container__inner">
+          <h2 className="section-title"> The Wallet </h2>
+          <img src={ laptop } alt="laptop"/>
+          <div className="description-container">
+            <h3 className="section-subtitle"> Dashboard Design </h3>
+            <p>When it comes to giving a FUCK, nothing is more important than keeping track of your movement.  With our FuckHub Dashboard you can monitor your balance, tips, burns, statistics and the leaderboard.</p>
+
+            <p className="spacer-small">The top tippers are prominently displayed for every user to see, offering instant bragging rights of being the biggest FUCKer in the world.  You can also show how little of a FUCK you give by climbing the top burner rate.</p>
+          </div>
+        </div>
+      </div>
+
+  </div>
 )
 
 
