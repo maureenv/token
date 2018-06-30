c6be4debb9979d835b424f314958c28e8f828110
maureenv <maureenvogel@outlook.com>
2018-06-30 14:23:38 -0400

    Work on grid section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,25 @@
  // npm run dev to start app
 import React from 'react'
 import Features from '../components/features'
-import fuck from '../../images/fuck.png'
-import fuckLogo from '../../images/fucktoken-circle-logo.png'
+
+import chart from '../../images/stats.png'
 import coinIcon from '../../images/icon-coins.png'
-import pieIcon from '../../images/icon-piechart.png'
+import customizable from '../../images/customizable.png'
+import discord from '../../images/discord.jpg'
+import fuck from '../../images/fuck.png'
 import fuckBlue from '../../images/fuck-blue.png'
+import fuckLogo from '../../images/fucktoken-circle-logo.png'
 import laptop from '../../images/laptop.png'
+import mail from '../../images/telegram.jpg'
+import mailPNG from '../../images/mail.png'
+import pieIcon from '../../images/icon-piechart.png'
+import reddit from '../../images/reddit.jpg'
+import secure from '../../images/secure.png'
+import slack from '../../images/slack.jpg'
+import twitch from '../../images/twitch.jpg'
+import twitter from '../../images/twitter.jpg'
+import wallet from '../../images/fuck-hub-wallet.png'
+import walletSmall from '../../images/nofees.png'
 
 
 const IndexPage = props => (
@@ -51,7 +64,62 @@ const IndexPage = props => (
         <div className="features-container__inner">
           <h2 className="section-title"> Features </h2>
             <h3 className="section-subtitle"> Connect all of your social media accounts to one tipping wallet </h3>
+            <div className="logos-container">
+              <div className="logo-column">
+                <img src={ twitter } alt="twitter"/>
+                <img src={ reddit } alt="reddit"/>
+                <img src={ discord } alt="discord"/>
+              </div>
+              <div className="logo-column">
+                <img className="wallet" src={ wallet } alt="wallet"/>
+              </div>
+              <div className="logo-column">
+                <img src={ mail } alt="mail"/>
+                <img src={ twitch } alt="twitch"/>
+                <img src={ slack } alt="slack"/>
+              </div>
+            </div>
             <button className="button button-purple auto-width"><img src={ pieIcon } alt="pie icon"/><p>Sign-Up For Fuckhub </p></button>
+        </div>
+      </div>
+      <div className="section-container gradient-bg">
+        <div className="section-title-container">
+          <h2 className="section-title"> A Closer Look </h2>
+          <h3 className="section-subtitle"> Connect all of your social media accounts to one tipping wallet </h3>
+          <div className="grid-container">
+            <div className="grid-container__inner">
+              <div className="grid-item">
+                <img src={ coinIcon } alt="coins"/>
+                <p className="title"> No Fees </p>
+                <p> We want the world to give a FUCK, so we offer no fees on your tips.  Gas costs are also free - we’ve got you covered. </p>
+              </div>
+              <div className="grid-item">
+                <img src={ chart } alt="chart"/>
+                <p className="title"> Personalized Stats </p>
+                <p> Want to know how often you give a FUCK?  Our charts and statistics can show exactly how much of a FUCK you give (or don’t). </p>
+              </div>
+              <div className="grid-item">
+                <img src={ walletSmall } alt="wallet"/>
+                <p className="title"> Account Creation </p>
+                <p> FuckHub wallets are automatically generated for new users.  Tipping someone a FUCK allows them to immediately join the fun, without the hassle of signing up. </p>
+              </div>
+              <div className="grid-item">
+                <img src={ secure } alt="secure"/>
+                <p className="title"> Secure </p>
+                <p> Your tipping wallet is more secure than Equifax.  Sign in with any supported platform and enable 2-Factor authorization to keep your FUCKs safe. </p>
+              </div>
+              <div className="grid-item">
+                <img src={ customizable } alt="customizable"/>
+                <p className="title"> Customizable </p>
+                <p> Become a top FUCKer and have your profile showcased on the dashboard of every other member.  Users can increase their rank based on the amount of tips they give. </p>
+              </div>
+              <div className="grid-item">
+                <img src={ mailPNG } alt="Mail"/>
+                <p className="title"> Free Withdrawals </p>
+                <p> Your wallet features free withdrawals from FuckHub.  Want to send your FUCKs to cold storage?  No problem, withdrawal couldn’t be easier. FUCKs safe. </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
